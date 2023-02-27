5877479282216f8add3d85d7e94205d15e45e0a2
machego <114348981+machego@users.noreply.github.com>
2023-02-27 19:04:05 +0100

    update data

--- Dog.js
+++ Dog.js
@@ -2,18 +2,18 @@ class Character{
     constructor(data) {
         Object.assign(this, data)
 
-        // property
     }
 
     // methods
     getCharacterHtml() {
         const { name, avatar, age, bio } = this
+        const isLiked = this.getStatusAction()
 
         return `
         <div class="character-card">
-
+            ${isLiked}
             <div class="character-info">
-                <h4 class="name"> ${name}, ${age} </h4>
+                <div class="name"> <b> ${name}, ${age} </b></div>
                 <div class="bio-dog">
                  ${bio}
                 </div>
@@ -22,8 +22,17 @@ class Character{
         </div>
         `
     }
+
+    getStatusAction() {
+    
+        return (!this.hasBeenLiked && this.hasBeenSwiped) ?
+        `<img class="reject-btn-img" src="./images/badge-nope.png">` 
+        : this.hasBeenLiked ?
+        `<img class="match-btn-img" src="./images/badge-like.png" alt="like-image">` 
+        :  ""
+    } 
+        
 }
 
 
-    
 export default Character
--- index.js
+++ index.js
@@ -4,12 +4,58 @@ import Character from './Dog.js'
 let dogIndex = 0
 let tinDog = new Character(characterData[dogIndex])
 
+let isWaiting = false
+
+function getNewDog(){
+    dogIndex = dogIndex + 1
+            if(dogIndex === 3 /*6*/ ) {
+            dogIndex = 0 
+    }
+    tinDog = new Character(characterData[dogIndex])
+
+}
+
+
+function like() {
+    if(!isWaiting) {
+        isWaiting = true
+        tinDog.hasBeenSwiped = true
+        tinDog.hasBeenLiked = true 
+        render()
+        setTimeout(() => {
+            getNewDog() 
+            isWaiting = false
+            render()
+        }, 1500)    
+    }   
+}
+
+function reject() {
+    if(!isWaiting) {
+        isWaiting = true
+        tinDog.hasBeenSwiped = true
+        tinDog.hasBeenLiked = false
+        render()
+        setTimeout(() => {
+            getNewDog()
+            isWaiting = false
+            render()
+        }, 1500)     
+    }
+}
+
 
 function render() {
     document.getElementById('card').innerHTML = tinDog.getCharacterHtml()
 
 }
 render()
+
+
+document.getElementById('reject-btn').addEventListener('click', reject);
+document.getElementById('match-btn').addEventListener('click', like);
+
+
 
 
 
